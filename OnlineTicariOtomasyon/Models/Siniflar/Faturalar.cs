28bab05674e43c54e9130a8cee2cab99cabfc7fa
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-10-02 18:10:21 +0300

    Add project files.

---
+++ OnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Faturalar
+    {
+        public int FaturaID { get; set; }
+        public char FaturaSeriNo { get; set; }
+        public string FaturaSiraNo { get; set; }
+        public DateTime Tarih { get; set; }
+        public DateTime Saat { get; set; }
+        public string TeslimEden { get; set; }
+        public string TeslimAlan{ get; set; }
+
+
+
+    }
+}
