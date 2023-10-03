441b435b4c7e26ffb0562a19ca51657225569a9a
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-10-03 14:16:59 +0300

    category add and update

--- OnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ OnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -18,5 +18,44 @@ namespace OnlineTicariOtomasyon.Controllers
             var degerler = c.Kategoris.ToList();
             return View(degerler);
         }
+
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            c.Kategoris.Remove(kategori);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var kategori = c.Kategoris.Find(k.KategoriID);
+            kategori.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
