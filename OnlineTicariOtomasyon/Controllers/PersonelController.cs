a7b698f58753c9efdc7ca0c8cd65d2294f6f8b0a
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-10-05 12:45:43 +0300

    personels are added

---
+++ OnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Siniflar;
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle( Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var personel = c.Personels.Find(id);
+            return View("PersonelGetir", personel);
+        }
+
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var personel = c.Personels.Find(p.PersonelID);
+            personel.PersonelAd = p.PersonelAd;
+            personel.PersonelSoyad = p.PersonelSoyad;
+            personel.PersonelGorsel = p.PersonelGorsel;
+            personel.Departmanid = p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
