7129b00a3a90e86a40722c44532cf3d7518521f0
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-10-08 20:18:25 +0300

    last updates

--- OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using OnlineTicariOtomasyon.Models.Siniflar;
 namespace OnlineTicariOtomasyon.Controllers
 {
     public class UrunDetayController : Controller
     {
         // GET: UrunDetay
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+
+            //var degerler = c.Uruns.Where(x => x.UrunID == 1).ToList();
+
+            cs.Deger1 = c.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Deger2 = c.Detays.Where(x => x.DetayID == 1).ToList();
+
+
+            return View(cs);
         }
     }
 }
