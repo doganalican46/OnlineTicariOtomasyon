15d76894ddb41574afe8fd108e1ed0146a1c168b
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-10-10 11:36:31 +0300

    sessions and authentications are added

---
+++ OnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OnlineTicariOtomasyon.Models.Siniflar;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var carimail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == carimail);
+            ViewBag.m = carimail;
+            return View(degerler);
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var carimail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == carimail.ToString()).Select(y => y.CariID).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
+
+            return View(degerler); 
+        }
+    }
+}
--- OnlineTicariOtomasyon/Controllers/LoginController.cs
+++ OnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using OnlineTicariOtomasyon.Models.Siniflar;
 
 
@@ -23,6 +24,8 @@ namespace OnlineTicariOtomasyon.Controllers
         {
             return PartialView();
         }
+
+
         [HttpPost]
         public PartialViewResult Partial1(Cariler cari)
         {
@@ -30,5 +33,52 @@ namespace OnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return PartialView();
         }
+
+
+        [HttpGet]
+        public ActionResult CariLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariLogin(Cariler cari)
+        {
+            var bilgi = c.Carilers.FirstOrDefault(x => x.CariMail == cari.CariMail && x.CariSifre == cari.CariSifre);
+            if(bilgi != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgi.CariMail,false);
+                Session["CariMail"] = bilgi.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index","Login");
+
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminLogin(Admin a)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.KullaniciAd == a.KullaniciAd && x.Sifre == a.Sifre);
+            if(bilgiler !=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.KullaniciAd, false);
+                Session["KullaniciAd"] = bilgiler.KullaniciAd.ToString();
+                return RedirectToAction("Index", "Kategori");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
     }
 }
