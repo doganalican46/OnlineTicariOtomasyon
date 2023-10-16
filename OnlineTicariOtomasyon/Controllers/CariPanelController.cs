cfcb5a13194c5c17c0e6531239068785f7f3d9af
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-10-16 20:28:33 +0300

    project finished

--- OnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ OnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -16,8 +16,24 @@ namespace OnlineTicariOtomasyon.Controllers
         public ActionResult Index()
         {
             var carimail = (string)Session["CariMail"];
-            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == carimail);
+            var degerler = c.Mesajlars.Where(x => x.Alici == carimail).ToList();
             ViewBag.m = carimail;
+            var mailid = c.Carilers.Where(x => x.CariMail == carimail).Select(y => y.CariID).FirstOrDefault();
+            ViewBag.mid = mailid;
+            var cariSehir = c.Carilers.Where(x => x.CariMail == carimail).Select(y => y.CariSehir).FirstOrDefault();
+            ViewBag.cariSehir = cariSehir;
+
+            var toplamSatis = c.SatisHarekets.Where(x => x.Cariid == mailid).Count();
+            ViewBag.toplamSatis = toplamSatis;
+
+            
+
+            //var toplamUrun = c.SatisHarekets.Where(x => x.Cariid == mailid).Sum(y => y.Adet);
+            //ViewBag.toplamUrun = toplamUrun;
+
+            var adsoyad = c.Carilers.Where(x => x.CariMail == carimail).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.adsoyad = adsoyad;
+
             return View(degerler);
         }
         [Authorize]
@@ -115,7 +131,28 @@ namespace OnlineTicariOtomasyon.Controllers
         {
             FormsAuthentication.SignOut();
             Session.Abandon();
-            return RedirectToAction("Index","Login");
+            return RedirectToAction("Index", "Login");
+        }
+
+
+        public PartialViewResult Partial1()
+        {
+            var carimail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == carimail).Select(y => y.CariID).FirstOrDefault();
+            var caribul = c.Carilers.Find(id);
+            return PartialView("Partial1",caribul);
+        }
+
+        public ActionResult CariBilgiGuncelle(Cariler cr)
+        {
+            var cari = c.Carilers.Find(cr.CariID);
+            cari.CariAd = cr.CariAd;
+            cari.CariSoyad = cr.CariSoyad;
+            cari.CariMail = cr.CariMail;
+            cari.CariSehir = cr.CariSehir;
+            cari.CariSifre = cr.CariSifre;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
